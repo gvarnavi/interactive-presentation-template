6e2df994e2e321a507337356749b8cb915a429af
gvarnavi <gvarnavi@mit.edu>
2023-12-28 22:20:06 -0800

    jupyterlab test

---
+++ src/components/JupyterCell.tsx
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021-2023 Datalayer, Inc.
+ *
+ * MIT License
+ */
+
+import React from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+        
+const JupyterCell = (props: any) => {
+  const { token, serverHttpUrl, serverWsUrl, source } = props;
+  return (
+    <BrowserOnly
+      fallback={<div>Jupyter Cell fallback content for prerendering.</div>}>
+      {() => {
+        // Keep the import via require in the BrowserOnly code block.
+        const { Jupyter } = require('@datalayer/jupyter-react/lib/jupyter/Jupyter');
+        const { Cell } = require('@datalayer/jupyter-react/lib/components/cell/Cell');
+        return (
+          <>
+            <Jupyter
+              jupyterToken={token}
+              jupyterServerHttpUrl={serverHttpUrl}
+              jupyterServerWsUrl={serverWsUrl}
+              disableCssLoading={true}
+            >
+              <Cell source={source}/>
+            </Jupyter>
+          </>
+        )
+      }}
+    </BrowserOnly>
+  )
+}
+
+export default JupyterCell;
